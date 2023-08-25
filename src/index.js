bf6dabbf0dcb8fb269f00228db5ece30e9ae3059
park <jh.parkssam@gmail.com>
2023-08-25 12:21:30 +0900

    hhhh

--- src/index.js
+++ src/index.js
@@ -11,10 +11,30 @@ import Videos from './pages/Videos';
 import VideoDetail from './pages/VideoDetail';
 import DetailPage from './component/DetailPage';
 
-const router = createBrowserRouter({
-  basename : process.env.PUBLIC_URL,
-  routes : [
-    {
+// const router = createBrowserRouter([
+
+//   {
+//     path : '/',
+//     element : <App/>,
+//     errorElement : <NotFound/>,
+//     children:[
+//       /*children : 중첩라우터 children으로 연결 내부에 있는 파일은 부모요소의 링크를
+//       기준으로 한다. 내부에 children으로 작성하면 되면 중첩 url을 생략할 수 
+//       있어서 문법이 간결해진다.
+//       */
+//      {path:'videos', element: <Videos/>},
+//      {path:'videos/:keyword', element : <Videos/>},//검색으로 나오는 결과물
+//      {path:'videos/watch/:videoId', element :<VideoDetail/>},
+     
+//     ]
+    
+//   },
+//     {path:'/movie/:movieId',element : <DetailPage/>}
+
+// ])
+
+const routes = [
+  {
     path : '/',
     element : <App/>,
     errorElement : <NotFound/>,
@@ -31,7 +51,12 @@ const router = createBrowserRouter({
     
   },
     {path:'/movie/:movieId',element : <DetailPage/>}
-],
+  
+]
+
+const router = createBrowserRouter({
+  basename: process.env.PUBLIC_URL,
+  routes: routes
 })
 
 
