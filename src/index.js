b9a3c843d17858fe25b6552f5877c270166f1821
park <jh.parkssam@gmail.com>
2023-08-25 13:03:05 +0900

    nvm

--- src/index.js
+++ src/index.js
@@ -10,31 +10,12 @@ import NotFound from './pages/NotFound';
 import Videos from './pages/Videos';
 import VideoDetail from './pages/VideoDetail';
 import DetailPage from './component/DetailPage';
-const router = createBrowserRouter([
 
-  {
-    path : `${process.env.PUBLIC_URL}`,
-    element : <App/>,
-    errorElement : <NotFound/>,
-    children:[
-      /*children : 중첩라우터 children으로 연결 내부에 있는 파일은 부모요소의 링크를
-      기준으로 한다. 내부에 children으로 작성하면 되면 중첩 url을 생략할 수 
-      있어서 문법이 간결해진다.
-      */
-     {path:'videos', element: <Videos/>},
-     {path:'videos/:keyword', element : <Videos/>},//검색으로 나오는 결과물
-     {path:'videos/watch/:videoId', element :<VideoDetail/>},
-     
-    ]
-    
-  },
-    {path:'/movie/:movieId',element : <DetailPage/>}
+const basename = process.env.PUBLIC_URL;
+// const router = createBrowserRouter([
 
-])
-
-// const routes = [
 //   {
-//     path : '/',
+//     path : `/`,
 //     element : <App/>,
 //     errorElement : <NotFound/>,
 //     children:[
@@ -50,19 +31,37 @@ const router = createBrowserRouter([
     
 //   },
 //     {path:'/movie/:movieId',element : <DetailPage/>}
-  
-// ]
 
-// const router = createBrowserRouter({
-//   basename: process.env.PUBLIC_URL,
-//   routes: routes
-// })
+// ])
+
+const routes = [
+  {
+    path : '/',
+    element : <App/>,
+    errorElement : <NotFound/>,
+    children:[
+      /*children : 중첩라우터 children으로 연결 내부에 있는 파일은 부모요소의 링크를
+      기준으로 한다. 내부에 children으로 작성하면 되면 중첩 url을 생략할 수 
+      있어서 문법이 간결해진다.
+      */
+     {path:'videos', element: <Videos/>},
+     {path:'videos/:keyword', element : <Videos/>},//검색으로 나오는 결과물
+     {path:'videos/watch/:videoId', element :<VideoDetail/>},
+     
+    ]
+    
+  },
+    {path:'/movie/:movieId',element : <DetailPage/>}
+  
+]
+
+const router = createBrowserRouter(routes,{basename:basename})
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router ={router} basename={process.env.PUBLIC_URL}/>
+    <RouterProvider router ={router} />
   </React.StrictMode>
 );
 
